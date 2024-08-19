42c6c235a81e341f5f3df919a24e81693691538a
piyushkumar4021 <piyushkumar4075@gmail.com>
2024-08-19 15:04:38 +0530

    Built a responsive layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import { Heading } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  return <Heading>GamesHome</Heading>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area={"nav"} bg="orange.300">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area={"aside"} bg="blue.300">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area={"main"} bg="green.300">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
