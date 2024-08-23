183fcb6a4c6cdcb15ab50f5d6a91cd29ad34c017
piyushkumar4021 <piyushkumar4075@gmail.com>
2024-08-23 11:07:12 +0530

    Displaying critic score

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,5 +1,6 @@
-import { Card, Image, CardBody, Heading } from "@chakra-ui/react";
+import { Card, Image, CardBody, Heading, HStack } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
+import CriticScore from "./CriticScore";
 
 function GameCard({ game }) {
   return (
@@ -7,9 +8,12 @@ function GameCard({ game }) {
       <Image src={game.background_image}></Image>
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
-        <PlatformIconList
-          platforms={game.parent_platforms.map((p) => p.platform)}
-        />
+        <HStack justifyContent="space-between">
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <CriticScore score={game.metacritic} />
+        </HStack>
       </CardBody>
     </Card>
   );
