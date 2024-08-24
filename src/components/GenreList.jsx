7f4a7c33cb143b09284f40d581d30309cc3502e2
piyushkumar4021 <piyushkumar4075@gmail.com>
2024-08-24 22:29:31 +0530

    Displaying genres

--- src/components/GenreList.jsx
+++ src/components/GenreList.jsx
@@ -1,14 +1,25 @@
+import { List, ListItem, HStack, Image, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
+import getCroppedImages from "../services/get-cropped-images";
 
 const GenreList = () => {
   const { data: genres } = useGenres();
 
   return (
-    <ul>
+    <List paddingX={4} paddingTop={5}>
       {genres.map((genre) => (
-        <li>{genre.name}</li>
+        <ListItem paddingY={1}>
+          <HStack>
+            <Image
+              src={getCroppedImages(genre.image_background)}
+              borderRadius="100%"
+              boxSize={9}
+            />
+            <Text>{genre.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
