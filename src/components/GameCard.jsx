f71974bb429df251e4cf5e23cc0c761b0d3b47e0
piyushkumar4021 <piyushkumar4075@gmail.com>
2024-08-24 22:17:27 +0530

    Bug fixed: card shadow not showing in light mode

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -2,24 +2,21 @@ import { Card, Image, CardBody, Heading, HStack } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImages from "../services/get-cropped-images";
-import GameCardContainer from "./GameCardContainer";
 
 function GameCard({ game }) {
   return (
-    <GameCardContainer>
-      <Card>
-        <Image src={getCroppedImages(game.background_image)}></Image>
-        <CardBody>
-          <Heading fontSize="2xl">{game.name}</Heading>
-          <HStack justifyContent="space-between">
-            <PlatformIconList
-              platforms={game.parent_platforms.map((p) => p.platform)}
-            />
-            <CriticScore score={game.metacritic} />
-          </HStack>
-        </CardBody>
-      </Card>
-    </GameCardContainer>
+    <Card borderRadius={10} overflow="hidden">
+      <Image src={getCroppedImages(game.background_image)}></Image>
+      <CardBody>
+        <Heading fontSize="2xl">{game.name}</Heading>
+        <HStack justifyContent="space-between">
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <CriticScore score={game.metacritic} />
+        </HStack>
+      </CardBody>
+    </Card>
   );
 }
 export default GameCard;
--- src/components/GameCardSkeleton.jsx
+++ src/components/GameCardSkeleton.jsx
@@ -1,16 +1,13 @@
 import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
-import GameCardContainer from "./GameCardContainer";
 
 function GameCardSkeleton() {
   return (
-    <GameCardContainer>
-      <Card>
-        <Skeleton height="300px" />
-        <CardBody>
-          <SkeletonText />
-        </CardBody>
-      </Card>
-    </GameCardContainer>
+    <Card borderRadius={10} overflow="hidden">
+      <Skeleton height="300px" />
+      <CardBody>
+        <SkeletonText />
+      </CardBody>
+    </Card>
   );
 }
 export default GameCardSkeleton;
