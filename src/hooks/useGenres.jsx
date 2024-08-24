d42fb4626ed8ce1940be1d3d82e8ff0350b03efa
piyushkumar4021 <piyushkumar4075@gmail.com>
2024-08-24 12:10:09 +0530

    Fetching genres

---
+++ src/hooks/useGenres.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+import config from "../config.json";
+import http from "../services/http-service";
+
+const useGenres = () => {
+  const [genres, setGenres] = useState([]);
+  const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    setLoading(true);
+
+    apiClient
+      .get(config.apiUrl + "/genres", { signal: controller.signal })
+      .then(({ data }) => {
+        setLoading(false);
+        setGenres(data.results);
+      })
+      .catch((err) => {
+        setLoading(false);
+
+        if (err instanceof http.CanceledError) return;
+        setError(err.message);
+      })
+      .finally(() => setLoading(false));
+
+    return () => controller.abort();
+  }, []);
+
+  return { genres, error, isLoading };
+};
+
+export default useGenres;
