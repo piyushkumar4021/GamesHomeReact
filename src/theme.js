094e51acea79f8d0c2ddc80c97fb5b3afdff1e61
piyushkumar4021 <piyushkumar4075@gmail.com>
2024-08-28 21:52:12 +0530

    Changed dark theme colors

--- src/theme.js
+++ src/theme.js
@@ -1,9 +1,21 @@
 import { extendTheme } from "@chakra-ui/react";
 
-const config = {
+const theme = extendTheme({
   initialColorMode: "dark",
-};
-
-const theme = extendTheme({ config });
+  colors: {
+    gray: {
+      50: "#f2f2f3",
+      100: "#d9d9d9",
+      200: "#bebebe",
+      300: "#a5a5a5",
+      400: "#8b8b8b",
+      500: "#727272",
+      600: "#585858",
+      700: "#3f3f3f",
+      800: "#262626",
+      900: "#0c0d0e",
+    },
+  },
+});
 
 export default theme;
